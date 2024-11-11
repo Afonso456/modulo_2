77b54f22c0e2aaa63a43daef0e5ed28226044d40
Afonso <ac00090@gmail.com>
2024-11-11 14:33:58 +0000

    11/11

--- desafio2_v4.py
+++ desafio2_v4.py
@@ -1,7 +1,7 @@
 carga= 0
-carga_maxima= 1000
 total= 0
-while carga < carga_maxima:
+while carga < 1000:
     peso= int(input("Insira o peso de cada mala em kg:"))
-    total= carga_maxima - peso
+    total= total + peso  
     print(total)
+    break
